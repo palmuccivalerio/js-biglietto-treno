90a3d2cae5be6ecbb8ae93e7c9dca0c27eeb29ec
palmuccivalerio <palmuccivalerio@gmail.com>
2024-03-25 16:20:24 +0100

    add:prezzo

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// INPUT
+let age = prompt("Quanti anni hai?"); // sting | null
+console.log( age, typeof age);
+age = parseInt(age); // number
+
+let numberskm = prompt("Numero kilometri?"); // sting | null
+console.log( numberskm, typeof numberskm);
+numberskm = parseInt(numberskm)
+
+const conversione= 21 / (100);
+console.log( conversione, typeof conversione);
+
+let prezzo = numberskm * conversione;
+console.log( prezzo, typeof prezzo);
+
+
+
+
+
+
+
